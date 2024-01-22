a288533ef267116d93d0c5cc550916abd04c38c9
J-guanghua <hua.163com@qq.com>
2024-01-22 19:30:44 +0800

    liupdate

--- stat.go
+++ stat.go
@@ -4,67 +4,59 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"sync"
 )
 
 type statTop struct {
-	once sync.Once
-	total uint64 // 总次数
-	errTotal   uint64 // 失败次数
-	keyTop map[string]int64
+	total    uint64 // 总次数
+	errTotal uint64 // 失败次数
+	keyTop   map[string]int64
 }
 
-func (s *statTop) before(envet Envet)  {
-	s.total ++
-	s.keyTop[envet.Key()] +=1
+func (s *statTop) before (envet Envet) {
+	s.total++
+	s.keyTop[envet.Key()]++
 }
-func (s *statTop) after(envet Envet)  {
+
+func (s *statTop) after (envet Envet) {
 	if envet.Err() != nil {
-		s.errTotal ++
+		s.errTotal++
 	}
 }
+
 type cacheStat struct {
-	total uint64 // 总次数
-	errTotal   uint64 // 失败次数
-	m sync.Mutex
-	errs map[string]int64
-	method map[string]*statTop // key top 统计分析
+	total    uint64 // 总次数
+	errTotal uint64 // 失败次数
+	errs     map[string]int64
+	method   map[string]*statTop // key top 统计分析
 }
 
 func NewStat() *cacheStat {
 	return &cacheStat{
 		errs: map[string]int64{},
 		method: map[string]*statTop{
-			"GET":&statTop{keyTop:make(map[string]int64,100)},
-			"SET":&statTop{keyTop:make(map[string]int64,100)},
-			"DELITE":&statTop{keyTop:make(map[string]int64,100)},
+			"GET":    {keyTop: make(map[string]int64, 100)},
+			"SET":    {keyTop: make(map[string]int64, 100)},
+			"DELITE": {keyTop: make(map[string]int64, 100)},
 		},
 	}
 }
 
-func (stat *cacheStat) before(ctx context.Context,envet Envet) error {
-	stat.total ++
+func (stat *cacheStat) before(ctx context.Context, envet Envet) error {
+	stat.total++
 	stat.method[envet.Method()].before(envet)
 	return nil
 }
 
-func (stat *cacheStat) after(ctx context.Context,envet Envet) {
-	if err := envet.Err();err != nil {
-		stat.errTotal ++
-		stat.errs[err.Error()] +=1
+func (stat *cacheStat) after(ctx context.Context, envet Envet) {
+	if err := envet.Err(); err != nil {
+		stat.errTotal++
+		stat.errs[err.Error()]++
 	}
-	//stat.m.Lock()
-	//defer stat.m.Unlock()
-	//log.Printf("stat: 总计数(%d),错误计数(%d),errs(%v)",stat.total,stat.errTotal,stat.errs)
-	//method := stat.method[envet.Method()]
-	//log.Printf("stat: method(%s),总计数(%d),错误计数(%d)",envet.Method(),method.total,method.errTotal)
-	//stat.method[envet.Method()].after(envet)
 }
 
 func (stat *cacheStat) HandleFunc(writer http.ResponseWriter, request *http.Request) {
 	log.SetOutput(writer)
-	log.Printf("stat: 总计数(%d),错误计数(%d),errs(%v)",stat.total,stat.errTotal,stat.errs)
-	log.Printf("stat: method(%v)",stat.method["GET"])
-	log.Printf("stat: method(%v)",stat.method["SET"])
-	return
+	log.Printf("stat: 总计数(%d),错误计数(%d),errs(%v)", stat.total, stat.errTotal, stat.errs)
+	log.Printf("stat: method(%v)", stat.method["GET"])
+	log.Printf("stat: method(%v)", stat.method["SET"])
 }
